17963d01156c16a8080a6c692ee8c0a31f63fb4e
AntPLos <antonio.loschi@gmail.com>
2026-05-22 13:21:33 -0300

    Adicionei as outras operacoes

--- calcSimp.pas
+++ calcSimp.pas
@@ -7,20 +7,48 @@ var
         Numero2 : Integer;
         Resultado: Real;
 begin
-        Writeln('Escreva qual operacao voce deseja realizar [+] [-] [*] [/]:');
+        Write('Escreva qual operacao voce deseja realizar [+] [-] [*] [/]:');
         Readln(Operacao);
-        Writeln('Escreva o primeiro numero inteiro:');
+        Writeln;
+        Write('Escreva o primeiro numero inteiro:');
         Readln(Numero1);
-        Writeln('Escreva o segundo numero inteiro:');
+        Writeln;
+        Write('Escreva o segundo numero inteiro:');
         Readln(Numero2);
+        Writeln;
 
         if Operacao = '+' then
         begin
-                Writeln(Numero1 + Numero2);
+                Resultado := Numero1 + Numero2;
+                Writeln(Resultado:0:0);
                 Readln;
         end
+
+        else if Operacao = '-' then
+        begin
+                Resultado := Numero1 - Numero2;
+                Writeln(Resultado:0:0);
+                Readln;
+        end
+
+        else if Operacao = '*' then
+        begin
+                Resultado := Numero1 * Numero2;
+                Writeln(Resultado:0:0);
+                Readln;
+        end
+
+        else if Operacao = '/' then
+        begin
+                Resultado := Numero1 / Numero2;
+                Writeln(Resultado:0:2);
+                Readln;
+       end
+
         else
-                Write('lula');
+                Write('Erro: digite apenas as opcoes apresentadas.');
+                Readln;
         end.
+
 
 end.
